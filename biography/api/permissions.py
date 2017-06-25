52a602f9d11e969e2bd0b4dc8e014e19259430fa
Sambeth <sambethslim@gmail.com>
2017-06-25 00:47:38 +0000

    change database to postgres, create test for api view, modified permissions import in permissions.py

--- biography/api/permissions.py
+++ biography/api/permissions.py
@@ -1,7 +1,7 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions 
 
 
-class IsOwnerOrReadOnly(BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
 	message = 'you must be the owner of this object'
 
 	def has_object_permission(self, request, view, obj):
---
+++ biography/tests/test_api_views.py
@@ -0,0 +1,81 @@
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User
+from ..models import Profile, Direct
+# from ..api.serializers import (
+# 	ProfileListSerializer,
+# 	ProfileDetailSerializer,
+# 	ProfileCreateUpdateSerializer,
+# 	DirectListSerializer,
+# 	DirectDetailSerializer,
+# 	DirectCreateUpdateSerializer,
+# 	)
+
+
+# ProfileListSerializer,
+# 	ProfileDetailSerializer,
+# 	ProfileCreateUpdateSerializer,
+# 	DirectListSerializer,
+# 	DirectDetailSerializer,
+# 	DirectCreateUpdateSerializer,
+
+# first_name = models.CharField(max_length=200)
+# 	last_name = models.CharField(max_length=200)
+# 	other_names = models.CharField(max_length=200)
+# 	date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+# 	nationality = models.CharField(max_length=200)
+# 	marital_status = models.CharField(max_length=11,
+# 		                              choices=STATUS_CHOICES,
+# 		                              default="SINGLE")
+
+
+class ProfileAPITestView(APITestCase):
+
+	def setUp(self):
+		self.client = APIClient(enforce_csrf_checks=True)
+		self.user = User.objects.create(username='phyll',
+										email='phyll@gmail.com',
+				                        is_active=True,
+				                        is_superuser=True,
+				                        is_staff=True)
+		self.client.force_authenticate(user=self.user)
+
+	def test_profile_api_can_create_a_bio(self):
+		url = reverse('bio-api:profile-create')
+		data = {'first_name': 'Kweku', 'last_name': 'Poku',
+				'other_names': 'Boafi', 'date_of_birth': '1992-02-12',
+				'nationality': 'Togolese', 'marital_status': 'MARRIED'}
+		response = self.client.post(url, data, format='json')
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+	def test_profile_api_get_a_bio(self):
+		p = Profile.objects.create(user=self.user, first_name='Phyllis',
+			                       last_name='Boatemaah', other_names='Sambeth',
+			                       date_of_birth='1992-12-30', nationality='Ghanaian',
+			                       marital_status='Single')
+		url = reverse('bio-api:profile-detail', kwargs={'pk': p.id})
+		response = self.client.get(url, format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertContains(response, p)
+
+	def test_profile_api_can_update_a_bio(self):
+		p = Profile.objects.create(user=self.user, first_name='Phyllis',
+			                       last_name='Boatemaah', other_names='Sambeth',
+			                       date_of_birth='1992-12-30', nationality='Ghanaian',
+			                       marital_status='Single')
+		data = {'first_name': 'Kweku', 'last_name': 'Poku',
+				'other_names': 'Boafi', 'date_of_birth': '1992-02-12',
+				'nationality': 'Togolese', 'marital_status': 'COMPLICATED'}
+		url = reverse('bio-api:profile-update', kwargs={'pk': p.id})
+		response = self.client.put(url, data, format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+	def test_profile_api_can_delete_a_bio(self):
+		p = Profile.objects.create(user=self.user, first_name='Phyllis',
+			                       last_name='Boatemaah', other_names='Sambeth',
+			                       date_of_birth='1992-12-30', nationality='Ghanaian',
+			                       marital_status='Single')
+		url = reverse('bio-api:profile-delete', kwargs={'pk': p.id})
+		response = self.client.delete(url, format='json', follow=True)
+		self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
