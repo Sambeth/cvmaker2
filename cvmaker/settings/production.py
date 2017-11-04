b4ce49819f46de5ac857473c6271da8d82c4dbb8
Samuel Baafi Boakye <sambethslim@gmail.com>
2017-11-04 09:34:32 +0000

    Update production.py

--- cvmaker/settings/production.py
+++ cvmaker/settings/production.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cvmaker_drf',
         'USER': 'postgres',
-        'PASSWORD': 'Pervert1',
+        'PASSWORD': 'Passowrd',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
